e40a89f3d73a205e44d85ee100f6af1aaff5cc4c
Jonathan Cooper <jonathan.cooper.2010@gmail.com>
2013-11-20 23:37:52 -0500

    Fixed up app.py

--- app.py
+++ app.py
@@ -13,7 +13,13 @@ app.jinja_env.add_extension('pyhaml_jinja.HamlExtension')
 # HACK WARNING:
 # We use this object to talk between the threads and this.
 diagnosis = None
-lock = object()
+
+
+def get_diagnosis(symptoms):
+  t = threading.Thread(target=diagnose_condition, args=(symptoms))
+  t.start()
+  t.join()
+  return diagnosis
 
 
 def diagnose_condition(*symptoms):
@@ -26,24 +32,17 @@ def diagnose_condition(*symptoms):
     symptoms = ['fever', 'swollenglands']
     t = threading.Thread(target=diagnose_condition, args=(symptoms))
     t.start()
-    wait_for_value(diagnosis)
-    do_other_stuff()
+    t.join()
+    print diagnosis
     >>> 'mumps'
   """
 
   global diagnosis
-  diagnosis = lock
   from doctor import Doctor
 
   d = Doctor('medical.pl')
   diagnosis = d.diagnose(symptoms)
   return
-
-
-def wait_for_value(obj):
-  # Busy wait until object is ready.
-  while obj == lock:
-    pass
 
 
 @app.route('/')
