778cb29ae5b52ad13018dfa98a345325b71433bb
Kevin Leyow <kleyow@jgxlabs.com>
2013-11-21 00:10:49 -0500

    Added handler to diagnose user's selected options.

--- static/js/question.js
+++ static/js/question.js
@@ -1,11 +1,4 @@
-var selected = new Array();
-
 $(document).ready(function() {
-	$(document).on('submit','form.question-form',function(){
-	  $("input:radio:checked").each(function() {
-	       selected.push($(this).val());
-	  });
-	});
 
 	$( "input.next" ).click(function() {
 	  if (curSlide < slideEls.length - 1) {
